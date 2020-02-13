4cac3de98b1a00da9e8f03575fcfed7a46849c3d
Raptor1001 <blueraptor1001@gmail.com>
2020-02-13 07:52:57 -0800

    complete sell-check component

---
+++ src/app/components/buy-sell/sell/sell-check/sell-check.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sell-check',
+  templateUrl: './sell-check.component.html',
+  styleUrls: ['./sell-check.component.css']
+})
+export class SellCheckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
